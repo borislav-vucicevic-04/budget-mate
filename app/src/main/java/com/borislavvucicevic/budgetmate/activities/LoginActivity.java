33a9592e96c261ef8028d72213b579ba54c963ca
borislav-vucicevic-04 <borislavvucicevic516@gmail.com>
2026-07-11 22:13:43 +0200

    Resolved issue: Login Activity - Developing UI #3

--- app/src/main/java/com/borislavvucicevic/budgetmate/activities/LoginActivity.java
+++ app/src/main/java/com/borislavvucicevic/budgetmate/activities/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.borislavvucicevic.budgetmate;
+package com.borislavvucicevic.budgetmate.activities;
 
 import android.os.Bundle;
 
@@ -8,16 +8,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.borislavvucicevic.budgetmate.R;
+
+public class LoginActivity extends AppCompatActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_login);
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activityLogin), (v, insets) -> {
-      Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-      v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+      int imeBottom = insets.getInsets(WindowInsetsCompat.Type.ime()).bottom;
+      v.setPadding(v.getPaddingLeft(), v.getPaddingTop(), v.getPaddingRight(), imeBottom);
       return insets;
     });
   }
